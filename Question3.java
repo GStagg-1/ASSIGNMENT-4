a5812beaba8859422f6d1d27ada0318f91253c58
GStagg-1 <gavinstagg22@gmail.com>
2025-05-05 13:15:57 -0700

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -7,12 +7,15 @@ public class Question3 {
 	public static void main(String[] args) {
 	
 		
-	
+	//Initialize sum variable
 		int sum = 0;
+		//increment variable "a" as long as it is <=10 
 		for(int a = 1; a <= 10; a++) {
+			//Use the loop to increment variable "sum" by a's value
 		sum+=a;
 	
 		}
+		//print sum
 		System.out.println("The sum of numbers 1-10 = " + sum);	
 	
 	}
